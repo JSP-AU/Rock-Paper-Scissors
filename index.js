7d9bff80ed3fdc29674ae250291c99412a4e7381
JSP-AU <joshuapollock6@gmail.com>
2023-05-23 20:26:34 +1000

    added a function called game that plays 5 rounds

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice (){
   const choice = options[Math.floor(Math.random()* options.length)];
-  console.log(choice);
   return(choice); 
 }
 
@@ -35,6 +34,13 @@ function playRound(playerSelection, computerSelection){
   }
 }
 
-const playerSelection = "paper";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+function game(){
+  console.log("Welcome!")
+  for (let i = 0; i < 5; i++){
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice(); 
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+game()
